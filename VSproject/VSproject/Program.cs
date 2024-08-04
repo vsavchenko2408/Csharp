b6fd1fbe75b7ba9334dfac7930ce910de3ab2d67
DerMeister <v.savchenko2408@gmail.com>
2024-08-04 19:33:25 +0300

    add vs project

---
+++ VSproject/VSproject/Program.cs
@@ -0,0 +1,29 @@
+﻿class MyClass
+{
+    private int x;
+    public MyClass(int x)
+    {
+    this.x = x;
+    }
+    public void set_x(int x)
+    {
+        this.x = x;
+    }
+    public int get_x()
+    {
+        return this.x;
+    }
+
+
+class Program
+{
+     static void Main()
+    {
+            MyClass a;
+            a.set_x(15);
+
+
+        
+
+    }
+}
