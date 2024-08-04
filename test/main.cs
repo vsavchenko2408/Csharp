3f129a2c4b4b0fd128a6b4dbf72375f731cb42e2
DerMeister <v.savchenko2408@gmail.com>
2024-08-04 22:07:10 +0300

    edit  test

--- test/main.cs
+++ test/main.cs
@@ -1,14 +1,12 @@
 using System;
-class MyClass
-{
-    
-}
+
 class Program
     {
     static void Main(string[] args)
         {
-         MyClass mc;
-         
+         MyClass mc = new MyClass(10);
+         //mc.set_data(15);
+         Console.WriteLine(mc.get_data());
 
 
         }
