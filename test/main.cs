ba4d6ef94e1d92797ce91fa38ddd99f887ce4b09
DerMeister <v.savchenko2408@gmail.com>
2024-08-04 21:50:41 +0300

    edit test

--- test/main.cs
+++ test/main.cs
@@ -1,12 +1,15 @@
 using System;
-
+class MyClass
+{
+    
+}
 class Program
     {
     static void Main(string[] args)
         {
-            int a = 10;
-            double b = 14.2;
-        Console.WriteLine("A: "+ a);
-        Console.WriteLine("B: " + b);
+         MyClass mc;
+         
+
+
         }
     }
