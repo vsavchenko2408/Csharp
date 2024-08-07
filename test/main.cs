883b87e778202679746e47de02e6ab3703440ee1
DerMeister <v.savchenko2408@gmail.com>
2024-08-07 13:35:28 +0300

    dual array

--- test/main.cs
+++ test/main.cs
@@ -4,12 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] arr = new int[3] {1,2,3};
-        
-
-        for(int i=0;i<3;++i)
+    int[,] arr = new int[,] 
         {
-            Console.WriteLine(arr[i]);
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+    for (int i=0; i<3; ++i)
+        {
+        for (int j=0;j<3;++j)
+            {
+            Console.Write(arr[i, j] + " ");
+            }
+            Console.WriteLine(" ");
         }
     }
 }
